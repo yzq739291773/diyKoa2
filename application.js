4e8c779b0d59e03fbe48cd47022b4da3afd077b4
yzq <739291773@qq.com>
2018-10-10 15:38:33 +0800

    完成精简版的koa2

--- application.js
+++ application.js
@@ -34,20 +34,36 @@ class Application {
         this.contex = contex
         this.request = request
         this.response = response
+        this.middlewares = []
     }
 
     use(callback) {
+        this.middlewares.push(callback)
         this.callback = callback
     }
+    compose(middlewares) {
+        return function(context) {
+            return dispatch(0)
 
+            function dispatch(i) {
+                let fn = middlewares[i]
+                if (!fn) {
+                    return Promise.resolve()
+                }
+                return Promise.resolve(fn(context, function next() {
+                    return dispatch(i + 1)
+                }))
+            }
+        }
+    }
     listen(...args) {
         const server = http.createServer(async(req, res) => {
             // console.log(2222, res._body)
             let ctx = this.createCtx(req, res)
-            await this.callback(ctx)
+            const fn = this.compose(this.middlewares)
+            await fn(ctx)
             console.log(333, ctx.body)
             ctx.res.end(ctx.body)
-                // this.callback(req, res)
         })
         server.listen(...args)
     }
--- server.js
+++ server.js
@@ -1,12 +1,34 @@
 const DiyKoa = require('./application')
 const app = new DiyKoa()
 
-app.use(async(ctx) => {
-    console.log(1111, ctx.url)
-    ctx.body = 'hello yzq' + ctx.url
-        // res.writeHead(200)
-        // res.end('hello yzq1')
+// app.use(async(ctx) => {
+//     console.log(1111, ctx.url)
+//     ctx.body = 'hello yzq' + ctx.url
+//         // res.writeHead(200)
+//         // res.end('hello yzq1')
+// })
+app.use(async(ctx, next) => {
+    ctx.body = '1'
+    await next()
+    ctx.body += '2'
 })
+app.use(async(ctx, next) => {
+    ctx.body += '3'
+    await delay()
+    await next()
+    ctx.body += '4'
+})
+app.use(async(ctx, next) => {
+    ctx.body += '5'
+})
+
+function delay() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, 2000)
+    })
+}
 
 app.listen(9092, () => {
     console.log('server start on port 9092')
