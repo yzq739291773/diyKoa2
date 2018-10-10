3fc05cd996017fea6ec05b0ddd616556d4d9f160
yzq <739291773@qq.com>
2018-10-10 15:27:38 +0800

    模拟compose功能

--- test.js
+++ test.js
@@ -1,14 +1,60 @@
-let demo = {
-    _name: 'yzq',
-    get _name() {
-        return '不给你'
-    },
-    set _name(val) {
-        console.log("设置值")
-        this._name = val
-    }
+async function fn1(next) {
+    console.log('fn1')
+    await next()
+    console.log('end fn1')
 }
 
-console.log(demo._name)
-    // demo._name = 'aaa'
-    // console.log(demo._name)
+async function fn2(next) {
+    console.log('fn2')
+    await delay()
+    await next()
+    console.log('end fn2')
+}
+
+async function fn3(next) {
+    console.log('fn3')
+}
+
+function delay() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve()
+        }, 2000)
+    })
+}
+
+function compose(middlewares) {
+    return function() {
+        return dispatch(0)
+
+        function dispatch(i) {
+            let fn = middlewares[i]
+            if (!fn) {
+                return Promise.resolve()
+            }
+            return Promise.resolve(fn(function next() {
+                return dispatch(i + 1)
+            }))
+        }
+    }
+}
+const middlewares = [fn1, fn2, fn3]
+const finalFn = compose(middlewares)
+finalFn()
+
+
+
+// let demo = {
+//     _name: 'yzq',
+//     get _name() {
+//         return '不给你'
+//     },
+//     set _name(val) {
+//         console.log("设置值")
+//         this._name = val
+//     }
+// }
+
+// console.log(demo._name)
+// demo._name = 'aaa'
+// console.log(demo._name)
